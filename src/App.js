3bf8300e8cfdcafc135942fa40c59ed18138f1a6
abid <shaikabid0102@gmail.com>
2020-03-08 19:50:27 +0530

    Created project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from '../src/components/home/home.js'
+
+function App() {
+  return (
+    <Home></Home>
+  );
+}
+
+export default App;
